1cbb9f5224908a325f4bf8c28f49714a04e0b5c2
Micah <mbgaffor@ncsu.edu>
2021-04-19 22:37:00 -0400

    added book relationship and template

--- website/library/models.py
+++ website/library/models.py
@@ -3,15 +3,15 @@ from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
   id = db.Column(db.Integer, primary_key=True)
-  first_name = db.Column(db.String(100))
-  last_name = db.Column(db.String(100))
-  email = db.Column(db.String(150), unique=True)
-  password = db.Column(db.String(150))
+  first_name = db.Column(db.String(100), nullable=False)
+  last_name = db.Column(db.String(100), nullable=False)
+  email = db.Column(db.String(150), unique=True, nullable=False)
+  password = db.Column(db.String(150), nullable=False)
+  books = db.relationship('Book', backref='user')
 
 class Book(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String(100), unique=True)
+  title = db.Column(db.String(100), unique=True, nullable=False)
   checked_out = db.Column(db.Boolean)
-  author = db.Column(db.String(100))
-
-
+  author = db.Column(db.String(100), nullable=False)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
--- website/library/views.py
+++ website/library/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
@@ -7,3 +7,12 @@ views = Blueprint('views', __name__)
 @login_required
 def home():
   return render_template("home.html", user=current_user)
+
+@views.route('/add_book', methods=['GET', 'POST'])
+@login_required
+def add_book():
+
+  # if request.method == 'POST':
+  #   title = request.form.get('title')
+
+  return render_template("add_book.html", user=current_user)
